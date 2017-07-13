4708deb80ec67c18a5f15e590e9e315e53e123ea
Unknown <alex.811@hotmail.fr>
2017-07-13 14:53:12 +0200

    Improve serializer coverage

---
+++ tests/test_cmresserializer.py
@@ -0,0 +1,81 @@
+import unittest
+import logging
+import datetime
+import os
+import sys
+import numpy as np
+from decimal import *
+
+sys.path.insert(0, os.path.abspath('.'))
+from cmreslogging.serializers import CMRESSerializer
+
+
+class CMRESSerializerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.log = logging.getLogger("MyTestCase")
+        self.formatter = logging.Formatter('%(asctime)s')
+
+    def tearDown(self):
+        del self.log
+        del self.formatter
+
+    def test_dumps_classic_log(self):
+        serializer = CMRESSerializer()
+        record = self.log.makeRecord(name=self.log.name,
+                                     level=logging.INFO,
+                                     fn=self.__class__.__name__,
+                                     lno=58, msg="dump_classic_log",
+                                     args=None, 
+                                     exc_info=False,
+                                     func=None,
+                                     extra=None)
+        self.formatter.format(record)
+        for value in record.__dict__.itervalues():
+          try:
+              serializer.dumps(value)
+          except TypeError:
+              self.fail("Serializer raised a TypeError exception")
+
+    def test_dumps_exception_log(self):
+        serializer = CMRESSerializer()
+        try:
+            badIdea = 1/0
+        except ZeroDivisionError:
+            record = self.log.makeRecord(name=self.log.name,
+                                         level=logging.ERROR,
+                                         fn=self.__class__.__name__,
+                                         lno=58, msg="dump_exception_log",
+                                         args=None, 
+                                         exc_info=sys.exc_info(),
+                                         func=None,
+                                         extra=None)
+        self.formatter.format(record)
+        for value in record.__dict__.itervalues():
+            try:
+                serializer.dumps(value)
+            except TypeError:
+                self.fail("Serializer raised a TypeError exception")
+
+    def test_dumps_log_with_extras_and_args(self):
+        serializer = CMRESSerializer()
+        record = self.log.makeRecord(name=self.log.name,
+                                     level=logging.ERROR,
+                                     fn=self.__class__.__name__,
+                                     lno=58, msg="dump_%s_log",
+                                     args="args", 
+                                     exc_info=False,
+                                     func=None,
+                                     extra={'complexvalue1': np.int64(1),
+                                            'complexvalue2': datetime.date.today(),
+                                            'complexvalue3': Decimal('3.0')})
+        self.formatter.format(record)
+        for value in record.__dict__.itervalues():
+            try:
+                serializer.dumps(value)
+            except TypeError:
+                self.fail("Serializer raised a TypeError exception")
+
+
+if __name__ == '__main__':
+  unittest.main()
